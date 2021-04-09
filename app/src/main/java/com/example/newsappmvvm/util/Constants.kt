a76da83c58096889d3aa3c17d4c8c09549807947
nikhilsutar123 <nkhlstr@gmail.com>
2021-04-09 10:46:15 +0530

    initial commit

---
+++ app/src/main/java/com/example/newsappmvvm/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsappmvvm.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "06b9594aa59844859f0fd77f8b168842"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
