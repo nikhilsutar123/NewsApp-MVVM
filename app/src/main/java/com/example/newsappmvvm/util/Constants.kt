0b81156138fb46f34e2e105cc4faa376cb1b297c
nikhilsutar123 <nkhlstr@gmail.com>
2021-05-09 13:08:40 +0530

    utility class for constants

--- app/src/main/java/com/example/newsappmvvm/util/Constants.kt
+++ app/src/main/java/com/example/newsappmvvm/util/Constants.kt
@@ -4,5 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "06b9594aa59844859f0fd77f8b168842"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_DELAY = 500L
+        const val SEARCH_QUERY_SIZE = 20
     }
 }
