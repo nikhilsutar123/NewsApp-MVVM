32fc001640ff6ff601e0013ae3f57897abfd0ef9
nikhilsutar123 <nkhlstr@gmail.com>
2021-05-09 13:07:15 +0530

    convert data from/to Source type

---
+++ app/src/main/java/com/example/newsappmvvm/data/Converters.kt
@@ -0,0 +1,16 @@
+package com.example.newsappmvvm.data
+
+import androidx.room.TypeConverter
+
+class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+}
